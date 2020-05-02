64fd5342dd6d1a9789ed6284f089b4d97198e2ef
MIlanGhimire <milanghimire1@gmail.com>
2020-05-02 20:34:15 +0545

    Fragments added.

---
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidStatusFragment.kt
@@ -0,0 +1,7 @@
+package com.milanghimire.covid19.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.milanghimire.covid19.R
+
+class CovidStatusFragment : Fragment(R.layout.fragment_covid_status) {
+}
