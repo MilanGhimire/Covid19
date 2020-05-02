a943a68fda814837b1b587bd84d91a3355714d2e
MIlanGhimire <milanghimire1@gmail.com>
2020-05-02 21:18:16 +0545

    Welcome screen added.

---
+++ app/src/main/java/com/milanghimire/covid19/ui/WelcomeScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.milanghimire.covid19.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.milanghimire.covid19.R
+
+class WelcomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
