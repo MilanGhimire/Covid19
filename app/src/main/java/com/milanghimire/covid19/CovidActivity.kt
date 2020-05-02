7dd97cf54d4df486bbc672abfed086d56443c3fa
MIlanGhimire <milanghimire1@gmail.com>
2020-05-02 21:05:25 +0545

    Navigation menu added.

--- app/src/main/java/com/milanghimire/covid19/CovidActivity.kt
+++ app/src/main/java/com/milanghimire/covid19/CovidActivity.kt
@@ -2,11 +2,16 @@ package com.milanghimire.covid19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_covid.*
 
 class CovidActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_covid)
+
+        bottomNavigationView.setupWithNavController(covidNavHostFragment.findNavController())
     }
 }
