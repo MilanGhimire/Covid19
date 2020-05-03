597e64c1341ee27fd35dda8c9566b032f3944978
MIlanGhimire <milanghimire1@gmail.com>
2020-05-03 22:30:34 +0545

    Android minSdkVersion changed to 21 since ripple is not supported in 19.

--- app/src/main/java/com/milanghimire/covid19/ui/WelcomeScreenActivity.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/WelcomeScreenActivity.kt
@@ -1,19 +1,41 @@
 package com.milanghimire.covid19.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.milanghimire.covid19.R
 
 class WelcomeScreenActivity : AppCompatActivity() {
 
+    private val TAG = "WelcomeScreenActivity"
+
+    private lateinit var btnStart: Button
+    private lateinit var context: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
+
+        context = this
 
         setupUI()
     }
 
     private fun setupUI() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(TAG, "setupUI()")
+
+        btnStart = findViewById(R.id.btnStart)
+
+        setupBtnStart()
+    }
+
+    private fun setupBtnStart() {
+        btnStart.setOnClickListener {
+            startActivity(Intent(context, CovidActivity::class.java))
+            finish()
+        }
     }
 }
