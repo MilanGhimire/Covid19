c669d87c422b22537a04eddb77f2e322b2167fa4
MIlanGhimire <milanghimire1@gmail.com>
2020-05-04 01:03:48 +0545

    Resource class added for Response handling. ViewModel data retriving done.

--- app/src/main/java/com/milanghimire/covid19/repository/CovidRepository.kt
+++ app/src/main/java/com/milanghimire/covid19/repository/CovidRepository.kt
@@ -1,8 +1,11 @@
 package com.milanghimire.covid19.repository
 
+import com.milanghimire.covid19.api.RetrofitInstance
 import com.milanghimire.covid19.db.CovidDatabase
 
 class CovidRepository(
     val db: CovidDatabase
 ) {
+    suspend fun getCovidStatus(countryName: String) =
+        RetrofitInstance.api.getStatusByCountry(countryName)
 }
