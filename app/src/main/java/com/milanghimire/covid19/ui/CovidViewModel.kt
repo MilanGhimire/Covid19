78846d766f45541f66fda23de24a8aa2fc01759a
MIlanGhimire <milanghimire1@gmail.com>
2020-05-04 01:03:48 +0545

    Resource class added for Response handling. ViewModel data retriving done.

--- app/src/main/java/com/milanghimire/covid19/ui/CovidViewModel.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/CovidViewModel.kt
@@ -1,9 +1,36 @@
 package com.milanghimire.covid19.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.milanghimire.covid19.db.models.CovidCountryResponse
 import com.milanghimire.covid19.repository.CovidRepository
+import com.milanghimire.covid19.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class CovidViewModel(
     val covidRepository: CovidRepository
 ) : ViewModel() {
+
+    val covidStatus: MutableLiveData<Resource<CovidCountryResponse>> = MutableLiveData()
+
+    init {
+        getCovidStatus("nepal")
+    }
+
+    fun getCovidStatus(countryName: String) = viewModelScope.launch {
+        covidStatus.postValue(Resource.Loading())
+        val response = covidRepository.getCovidStatus(countryName)
+        covidStatus.postValue(handleCovidStatusResponse(response))
+    }
+
+    private fun handleCovidStatusResponse(response: Response<CovidCountryResponse>) : Resource<CovidCountryResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
--- app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidStatusFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidStatusFragment.kt
@@ -1,19 +1,43 @@
 package com.milanghimire.covid19.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.milanghimire.covid19.R
 import com.milanghimire.covid19.ui.CovidActivity
 import com.milanghimire.covid19.ui.CovidViewModel
+import com.milanghimire.covid19.util.Resource
 
 class CovidStatusFragment : Fragment(R.layout.fragment_covid_status) {
 
     lateinit var viewModel: CovidViewModel
 
+    private val TAG = "CovidStatusFragment"
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as CovidActivity).viewModel
+
+        viewModel.covidStatus.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+//                    ?.let is used for checking if response in not null
+                    response.data?.let {
+                        Log.d(TAG, "Data is retrived from API.")
+                    }
+                }
+                is Resource.Error -> {
+                    response.message?.let { message ->
+                        Log.d(TAG, "Error occured. $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    Log.d(TAG, "Request is Loading.")
+                }
+            }
+        })
     }
 }
