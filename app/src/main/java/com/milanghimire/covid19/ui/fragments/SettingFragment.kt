5fd8d43ca81ae9a8c01213333a9908fe5562852d
MIlanGhimire <milanghimire1@gmail.com>
2020-05-04 15:53:41 +0545

    Country loaded to Spinner.

--- app/src/main/java/com/milanghimire/covid19/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/SettingFragment.kt
@@ -1,7 +1,27 @@
 package com.milanghimire.covid19.ui.fragments
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.milanghimire.covid19.R
+import com.milanghimire.covid19.ui.CovidActivity
+import com.milanghimire.covid19.util.AdapterHelper
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        spinnerCountrySetting.adapter =
+            AdapterHelper.createAdapter(
+                (context as CovidActivity),
+                resources.getStringArray(R.array.countries_array).toList()
+            )
+    }
 }
