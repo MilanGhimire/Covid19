d53a4b890411f14bcaf4cc59ea1a80febb21f283
MIlanGhimire <milanghimire1@gmail.com>
2020-05-05 00:56:57 +0545

    Icon added provided by Ghanashyam bro. Fixed issue of News API.

--- app/src/main/java/com/milanghimire/covid19/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/NewsViewModelProviderFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.milanghimire.covid19.repository.NewsRepository
 
+@Suppress("UNCHECKED_CAST")
 class NewsViewModelProviderFactory(
     val newsRepository: NewsRepository
 ) : ViewModelProvider.Factory {
