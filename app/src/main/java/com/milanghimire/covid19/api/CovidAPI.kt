a42222fc36da19a16b95f79c9c02b18fcf147f29
MIlanGhimire <milanghimire1@gmail.com>
2020-05-04 13:02:44 +0545

    Today data load and temporary icon added.

--- app/src/main/java/com/milanghimire/covid19/api/CovidAPI.kt
+++ app/src/main/java/com/milanghimire/covid19/api/CovidAPI.kt
@@ -2,14 +2,16 @@ package com.milanghimire.covid19.api
 
 import com.milanghimire.covid19.db.models.CovidCountryResponse
 import retrofit2.Response
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CovidAPI {
 
-    @GET("/v2/nepal...")
+    @GET("v2/countries/{countryName}")
     suspend fun getStatusByCountry(
-        @Query("country")
+        @Path("countryName")
         countryName: String = "nepal"
     ): Response<CovidCountryResponse>
 }
--- app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidStatusFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidStatusFragment.kt
@@ -3,18 +3,29 @@ package com.milanghimire.covid19.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.milanghimire.covid19.R
+import com.milanghimire.covid19.db.models.CovidCountryResponse
 import com.milanghimire.covid19.ui.CovidActivity
 import com.milanghimire.covid19.ui.CovidViewModel
 import com.milanghimire.covid19.util.Resource
+import kotlinx.android.synthetic.main.fragment_covid_status.*
 
 class CovidStatusFragment : Fragment(R.layout.fragment_covid_status) {
 
     lateinit var viewModel: CovidViewModel
 
     private val TAG = "CovidStatusFragment"
+
+//    private lateinit var tvTests: TextView
+//    private lateinit var tvAffected: TextView
+//    private lateinit var tvActive: TextView
+//    private lateinit var tvRecovered: TextView
+//    private lateinit var tvCritical: TextView
+//    private lateinit var tvDeath: TextView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,7 +37,8 @@ class CovidStatusFragment : Fragment(R.layout.fragment_covid_status) {
                 is Resource.Success -> {
 //                    ?.let is used for checking if response in not null
                     response.data?.let {
-                        Log.d(TAG, "Data is retrived from API.")
+                        Log.d(TAG, "Data is retrived from API. ${response.data}")
+                        updateStatusData(response.data)
                     }
                 }
                 is Resource.Error -> {
@@ -39,5 +51,17 @@ class CovidStatusFragment : Fragment(R.layout.fragment_covid_status) {
                 }
             }
         })
+    }
+
+    private fun updateStatusData(status: CovidCountryResponse) {
+        // initialize the UI views
+        tvTests.text = status.tests.toString()
+        tvAffected.text = status.todayCases.toString()
+        tvActive.text = status.active.toString()
+        tvRecovered.text = status.recovered.toString()
+        tvCritical.text = status.critical.toString()
+        tvDeath.text = status.todayDeaths.toString()
+
+//        Glide.with()
     }
 }
