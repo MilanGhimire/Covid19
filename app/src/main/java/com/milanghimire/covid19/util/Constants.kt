0450506edf9bf99830a2d5df7ce68f35d98afbec
MIlanGhimire <milanghimire1@gmail.com>
2020-05-05 01:47:06 +0545

    Network policy .xml added but since above 24 only support haven't added intead used https for News API.

--- app/src/main/java/com/milanghimire/covid19/util/Constants.kt
+++ app/src/main/java/com/milanghimire/covid19/util/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
     companion object {
         const val COVID_BASE_URL = "https://corona.lmao.ninja/"
         const val NEWS_API_KEY = "b2d9cfdde84943b6aa901e91383f5533"
-        const val NEWS_BASE_URL = "http://newsapi.org/"
+        const val NEWS_BASE_URL = "https://newsapi.org/"
     }
 }
