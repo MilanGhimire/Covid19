5318474876a2004e04c626c2159f41f0213fc7a3
MIlanGhimire <milanghimire1@gmail.com>
2020-05-04 22:04:27 +0545

    Repository, ViewModel, ViewModelProviderFactory created.

--- app/src/main/java/com/milanghimire/covid19/ui/CovidActivity.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/CovidActivity.kt
@@ -8,19 +8,25 @@ import androidx.navigation.ui.setupWithNavController
 import com.milanghimire.covid19.R
 import com.milanghimire.covid19.db.CovidDatabase
 import com.milanghimire.covid19.repository.CovidRepository
+import com.milanghimire.covid19.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_covid.*
 
 class CovidActivity : AppCompatActivity() {
 
-    lateinit var viewModel: CovidViewModel
+    lateinit var covidViewModel: CovidViewModel
+    lateinit var newsViewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_covid)
 
         val covidRepository = CovidRepository(CovidDatabase(this))
-        val viewModelProviderFactory = CovidViewModelProviderFactory(covidRepository)
-        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(CovidViewModel::class.java)
+        val covidViewModelProviderFactory = CovidViewModelProviderFactory(covidRepository)
+        covidViewModel = ViewModelProvider(this, covidViewModelProviderFactory).get(CovidViewModel::class.java)
+
+        val newsRepository = NewsRepository()
+        val newsViewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        newsViewModel = ViewModelProvider(this, newsViewModelProviderFactory).get(NewsViewModel::class.java)
 
         bottomNavigationView.setupWithNavController(covidNavHostFragment.findNavController())
     }
--- app/src/main/java/com/milanghimire/covid19/ui/fragments/LatestNewsFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/LatestNewsFragment.kt
@@ -1,7 +1,19 @@
 package com.milanghimire.covid19.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.milanghimire.covid19.R
+import com.milanghimire.covid19.ui.CovidActivity
+import com.milanghimire.covid19.ui.NewsViewModel
 
 class LatestNewsFragment : Fragment(R.layout.fragment_latest_news) {
+
+    lateinit var newsViewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        newsViewModel = (activity as CovidActivity).newsViewModel
+    }
 }
