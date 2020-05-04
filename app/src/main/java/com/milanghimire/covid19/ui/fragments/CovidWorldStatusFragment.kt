c5b6d3e8d58ad61411b5fca537e29448ab86d985
MIlanGhimire <milanghimire1@gmail.com>
2020-05-05 01:54:18 +0545

    removed saved news and replaced with world status fragment.

--- app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidWorldStatusFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/CovidWorldStatusFragment.kt
@@ -3,5 +3,5 @@ package com.milanghimire.covid19.ui.fragments
 import androidx.fragment.app.Fragment
 import com.milanghimire.covid19.R
 
-class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+class CovidWorldStatusFragment : Fragment(R.layout.fragment_covid_world_status) {
 }
