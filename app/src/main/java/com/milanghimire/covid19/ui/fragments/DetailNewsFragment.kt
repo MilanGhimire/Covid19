d810d2e6dfd9fc0fc720a9dc7af38567f5625fe7
MIlanGhimire <milanghimire1@gmail.com>
2020-05-05 23:24:34 +0545

    View detail news and jvm version changed to 1.8 from 1.6

--- app/src/main/java/com/milanghimire/covid19/ui/fragments/DetailNewsFragment.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/fragments/DetailNewsFragment.kt
@@ -2,18 +2,27 @@ package com.milanghimire.covid19.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.milanghimire.covid19.R
 import com.milanghimire.covid19.ui.CovidActivity
 import com.milanghimire.covid19.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_detail_news.*
 
 class DetailNewsFragment : Fragment(R.layout.fragment_detail_news) {
 
     lateinit var newsViewModel: NewsViewModel
+    val args: DetailNewsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         newsViewModel = (activity as CovidActivity).newsViewModel
+        val article = args.article
+        webViewArticle.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
