a3630e821fff8887bcb8a5c278e90a4cc5c8bf65
MIlanGhimire <milanghimire1@gmail.com>
2020-05-07 00:32:00 +0545

    Splash screen with hide status bar done

--- app/src/main/java/com/milanghimire/covid19/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/milanghimire/covid19/ui/splashscreen/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.milanghimire.covid19.R
 import com.milanghimire.covid19.ui.CovidActivity
@@ -12,6 +13,8 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // for creating full screen
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splashscreen)
 
         Handler().postDelayed({
